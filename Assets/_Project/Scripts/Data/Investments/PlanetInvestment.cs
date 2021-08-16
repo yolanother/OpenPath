03f058e3dca330cf55a12a2ecb6dba4d812e6adb
Ross Gardler <ross@gardler.org>
2021-08-16 13:37:58 -0700

    Auto generate initial investment opportunities

--- Assets/_Project/Scripts/Data/Investments/PlanetInvestment.cs
+++ Assets/_Project/Scripts/Data/Investments/PlanetInvestment.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using DoubTech.OpenPath.Data.SolarSystemScope;
+using System;
 
 namespace DoubTech.OpenPath.Data.Investments
 {
@@ -14,5 +16,27 @@ namespace DoubTech.OpenPath.Data.Investments
         internal float requiredInvestmentAmount = 10000;
         [SerializeField, Range(0.01f, 1f), Tooltip("Impact on healthcare in terms of the number of normalized points to increase the healthcare quality.")]
         internal float healthcareIncrease = 0;
+
+        /// <summary>
+        /// Calculate a chance of this investment being available on a given planet.
+        /// </summary>
+        /// <param name="planetData">The data object describing the planet.</param>
+        /// <returns>A value between 0 (no chance) and 1 (</returns>
+        internal float Chance(Planet planetData)
+        {
+            if (planetData.Population <= 0)
+            {
+                return 0;
+            }
+
+            float chance = 0;
+            if (planetData.HealthcareQuality < 1)
+            {
+                chance +=  0.4f + planetData.Population / 10000;
+            }
+
+            return Mathf.Clamp01(chance);
+
+        }
     }
 }
